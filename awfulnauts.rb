4108f18bd419fe788290bd0dd87b67f987909413
Chris Kincanon <Chris.Kincanon@gmail.com>
2012-05-28 03:37:54 -0700

    addin in layout

--- awfulnauts.rb
+++ awfulnauts.rb
@@ -6,16 +6,6 @@ layout :layout
 
 #Home Page
 get '/' do
-#  @gamebreaker = Rss.get_gamebreaker
-#  @darth_cast = Rss.get_darthhater_cast
-
-#  @gamebreaker = Rss.get_feed('gamebreaker')
-#  @darth_cast = Rss.get_feed('darthhater_cast')
-#  @darthhater = Rss.get_feed('darthhater')
-#  @devtracker = Rss.get_feed('devtracker')
-#  @guild_twitter = Rss.get_feed('guild_twitter')
-#  @guild_reddit = Rss.get_feed('guild_reddit')
-#  @swtor_reddit = Rss.get_feed('swtor_reddit')
   haml :index
 end
 
