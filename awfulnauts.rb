dfb76e5a06ef01b26bff58d998553c779d0a06a4
Chris Kincanon <Chris.Kincanon@gmail.com>
2012-05-28 03:25:23 -0700

    defacing website

--- awfulnauts.rb
+++ awfulnauts.rb
@@ -2,20 +2,20 @@
 
 ### CONTROLLER ACTIONS
 
-layout :layout
+#layout :layout
 
 #Home Page
 get '/' do
 #  @gamebreaker = Rss.get_gamebreaker
 #  @darth_cast = Rss.get_darthhater_cast
 
-  @gamebreaker = Rss.get_feed('gamebreaker')
-  @darth_cast = Rss.get_feed('darthhater_cast')
-  @darthhater = Rss.get_feed('darthhater')
-  @devtracker = Rss.get_feed('devtracker')
-  @guild_twitter = Rss.get_feed('guild_twitter')
-  @guild_reddit = Rss.get_feed('guild_reddit')
-  @swtor_reddit = Rss.get_feed('swtor_reddit')
+#  @gamebreaker = Rss.get_feed('gamebreaker')
+#  @darth_cast = Rss.get_feed('darthhater_cast')
+#  @darthhater = Rss.get_feed('darthhater')
+#  @devtracker = Rss.get_feed('devtracker')
+#  @guild_twitter = Rss.get_feed('guild_twitter')
+#  @guild_reddit = Rss.get_feed('guild_reddit')
+#  @swtor_reddit = Rss.get_feed('swtor_reddit')
   haml :index
 end
 
